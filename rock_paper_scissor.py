46aa612d72f7d8d6d9455f939e4b701185e4f6f0
babayl0729 <babayl0729@gmail.com>
2023-12-24 18:00:38 -0700

    added comments about app's functionality

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,3 +1,21 @@
+# rock,paper,scissor
+# This application is designed to play rock, paper, scissor.
+# In this application the player will be prompt to enter
+# either rock, paper, or scissor, once the player picked
+# his/her moved the computer will ramdomly picked its moved,
+# and once the computer picked its moved the applocation will
+# prompt the user who wins.
+# The application uses a radom int to generate pseudo numbers.
+# The numbers represents as 0=rock, 1=paper, 2=scissor.
+# Another function were used in this application is input.
+# the input function were used for the user to enter its move.
+# Since the input function only returns string, the input was
+# casted using int() so that the user will not get an error
+# when entering his/her moved. Also conditional statement were
+# used in this application for to determine the move of the user
+# and computer.
+
+
 import random
 
 print("...enter 0 for rock...")
