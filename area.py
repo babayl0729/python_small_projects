04f688468737863627dbcbc3fdedd7858292ff2a
babayl0729 <babayl0729@gmail.com>
2024-02-03 01:46:38 -0700

    aadded trapezoid and ellipse

--- area.py
+++ area.py
@@ -30,14 +30,35 @@ def rectangle(num1,num2):
     except ValueError:
         return "That's not a number."
     area = float(num1 * num2)
-    return f"The area of rectangle: "
+    return f"The area of rectangle: {area}"
+
+def trapezoid(num1,num2,num3):
+    try:
+        num1 = float(input("Enter base a: "))
+        num2 = float(input("Enter base b: "))
+        num3 = float(input("Enter height: "))
+    except ValueError:
+        return "That's not a number."
+    area = float(num1 + num2)/2 * (num3)
+    return f"The area of trapezoid: {area}"
+
+def ellipse(num1,num2):
+    try:
+        num1 = float(input("Enter axis a: "))
+        num2 = float(input("Enter axis b: "))
+    except ValueError:
+        return "That's not a number."
+    area = float(3.14 * num1 * num2)
+    return f"The area of ellipse: {area}"
 
 while True:
     print("1. Circle")
     print("2. Square")
     print("3. Triangle")
     print("4. Rectangle")
-    print("5. Quit")
+    print("5. Trapezoid")
+    print("6. Ellipse")
+    print("7. Quit")
 
     number = int(input("Enter the number of shape that you want to get the Area: "))
 
@@ -54,9 +75,15 @@ while True:
         print("You picked Rectangle")
         print(rectangle("",""))
     elif number == 5:
+        print("You picked Trapezoid")
+        print(trapezoid("","",""))
+    elif number == 6:
+        print("You picked Ellipse")
+        print(ellipse("",""))
+    elif number == 7:
         print("Goodbye...")
         break
     else:
         print("Invalid entry!")
-        print("Pick 5 to quit")
+        print("Pick 7 to quit")
     
